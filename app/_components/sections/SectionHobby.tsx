e599707dd8345b31006bfeb29cbd6a601d6b6421
porink0424 <daichi5967@gmail.com>
2023-10-25 22:49:55 +0900

    Implement 'Hobby' Section

---
+++ app/_components/sections/SectionHobby.tsx
@@ -0,0 +1,42 @@
+import { Language } from '@/app/_constants/language'
+import { SECTION_HOBBY } from '@/app/_contents/SectionHobby'
+import { parseText } from '@/app/_utils/parseText'
+import {
+  Title,
+  Stack,
+  Accordion,
+  AccordionItem,
+  AccordionControl,
+  AccordionPanel,
+} from '@mantine/core'
+
+type Props = {
+  language: Language
+  sectionRef: React.RefObject<HTMLDivElement>
+}
+
+export default function SectionHobby({ language, sectionRef }: Props) {
+  return (
+    <Stack gap={48} ref={sectionRef} id='Hobby'>
+      <Title order={1} className='!text-5xl'>
+        Hobby
+      </Title>
+
+      <Accordion multiple className='px-4'>
+        {SECTION_HOBBY.main[language].map(({ title, content }, index) => (
+          <AccordionItem key={index} value={title}>
+            <AccordionControl
+              icon={SECTION_HOBBY.main.icons[index]}
+              classNames={{
+                icon: 'text-3xl',
+              }}
+            >
+              {title}
+            </AccordionControl>
+            <AccordionPanel>{parseText(content)}</AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </Stack>
+  )
+}
