19242b3e164e33f1bde45eb38089f85912531166
porink0424 <daichi5967@gmail.com>
2023-10-25 22:32:29 +0900

    Implement layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import '@mantine/core/styles.css'
-import { AppShell, AppShellMain, ColorSchemeScript, MantineProvider } from '@mantine/core'
+import { AppShell, AppShellMain, Box, ColorSchemeScript, MantineProvider } from '@mantine/core'
+import { Noto_Sans_JP } from 'next/font/google'
 import Header from './_components/Header'
 import Footer from './_components/Footer'
+import Providers from './providers'
+
+const notojp = Noto_Sans_JP({
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Daichi Kato',
@@ -12,9 +20,13 @@ export const metadata: Metadata = {
 
 function PageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AppShell header={{ height: 60 }} padding='md'>
+    <AppShell header={{ height: 80 }}>
       <Header />
-      <AppShellMain className='bg-white shadow-md'>{children}</AppShellMain>
+      <AppShellMain className='bg-white shadow-xl z-10 relative'>
+        <Box maw={720} mx='auto'>
+          {children}
+        </Box>
+      </AppShellMain>
       <Footer />
     </AppShell>
   )
@@ -27,10 +39,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta charSet='utf-8' />
         <ColorSchemeScript />
       </head>
-      <body>
-        <MantineProvider>
-          <PageLayout>{children}</PageLayout>
-        </MantineProvider>
+      <body className={notojp.className}>
+        <Providers>
+          <MantineProvider>
+            <PageLayout>{children}</PageLayout>
+          </MantineProvider>
+        </Providers>
       </body>
     </html>
   )
