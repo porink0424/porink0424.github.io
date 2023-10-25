04eb0ed061fd24311cd879267194232b3f1aab04
porink0424 <daichi5967@gmail.com>
2023-10-25 22:03:33 +0900

    Make atoms of activeSection & language

---
+++ app/_stores/atoms.ts
@@ -0,0 +1,5 @@
+import { atom } from 'jotai'
+import { LANGUAGES, Language } from '../_constants/language'
+
+export const activeSectionAtom = atom(0)
+export const languageAtom = atom<Language>(LANGUAGES.JP)
